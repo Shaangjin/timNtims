90bd4254d338688e477e7b7b074434f5454e796a
Seokjun Kang <kangsj0327@gmail.com>
2023-11-25 18:33:57 +0900

    modify error in import

--- app/src/main/java/seoultech/itm/timntims/adapter/ChatAdapter.java
+++ app/src/main/java/seoultech/itm/timntims/adapter/ChatAdapter.java
@@ -13,7 +13,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-import seoultech.itm.timntims.ImageClassification;
 import seoultech.itm.timntims.R;
 import seoultech.itm.timntims.model.ChatItem;
 import seoultech.itm.timntims.model.ImageItem;
